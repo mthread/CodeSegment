aeb630128330e7382b31ecf09e43ec4ce4720f4e
cs <chshuai.mail@gmail.com>
2014-11-17 09:59:50 +0800

    Update convexhull.lua

--- lua/convexhull/convexhull.lua
+++ lua/convexhull/convexhull.lua
@@ -14,7 +14,7 @@ end
 
 local function cross(p1,p2,p3)
  	return 	(p2.x - p1.x)*(p3.y - p1.y)
-   		-	(p2.y - p1.y)*(p3.x - p1.x) 
+   	      - (p2.y - p1.y)*(p3.x - p1.x) 
 end 
 
 local function getLeftIndex(points)
